fe02170e3267f7af44fca6d0d8da34dbeafcdc8d
bjornlecis <bjorn.lecis@gmail.com>
2024-01-22 20:56:09 +0100

    encrypt.py

---
+++ Hex.py
@@ -0,0 +1,8 @@
+dec = 999
+hexi = hex(dec)
+hexi = str(hexi[2:])
+print(hexi)
+
+c ="P"
+print(ord(c))
+print(chr(68),chr(65),chr(71))
---
+++ binary.py
@@ -0,0 +1,6 @@
+a = 79
+
+# Base 2(binary)
+bin_a = bin(a)
+print(bin_a)
+print(int(bin_a, 2)) #Base 2(binary)
